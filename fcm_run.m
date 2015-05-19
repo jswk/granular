cb7137b1d3b3f5c27bde72388e362f2a676f0086
Jakub Sawicki <kuba@jswk.pl>
2015-05-19 08:54:26 +0200

    Added stop condition based on difference between successive U values.

--- fcm_run.m
+++ fcm_run.m
@@ -1,9 +1,19 @@
-function [ V, U, Q ] = fcm_run( data, c, m, dist )
+function [ V, U, Q, Q_m ] = fcm_run( data, c, m, dist, eps )
 U = generate_random_U(size(data,1), c);
 sigmas = calc_sigmas(data);
-for i = 1:100
+step = 1;
+Q_m = [];
+stop = false;
+while not(stop);
+    U_prev = U;
     [U, V] = fcm_step(data, sigmas, U, m, dist);
     Q = calc_Q(data, U, V, dist, m);
+    Q_m(step) = Q;
+    step = step + 1;
+    diff_U = U - U_prev;
+    if max(diff_U(:).^2) < eps
+        stop = true;
+    end
 end
 end
 
