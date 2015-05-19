40c9fff9b85060b5aeebab2ef937bc62bf3989e2
Jakub Sawicki <kuba@jswk.pl>
2015-05-19 09:20:23 +0200

    Incomplete gen_G.m

---
+++ gen_G.m
@@ -0,0 +1,15 @@
+function [ G ] = gen_G( data, V, U )
+N = size(data, 1);
+c = size(V, 1);
+G = zeros(N, c);
+for i = 1:N
+    for j = 1:c
+        z = 
+        g = 
+        G(i,j) = g
+    end
+end
+
+
+end
+
